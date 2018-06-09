1b27021672ad0fff4885dcf63c7aa120a9c253f3
zhengshaodong <ngxcorpio@gmail.com>
2018-06-09 10:51:50 +0800

    upgrade scala to 2.12.6 and sbt to 1.1.6

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -5,6 +5,6 @@ object BuildSettings {
   lazy val commonSettings = Seq(
     organization := "cc.akkaha",
     version := "0.1.0-SNAPSHOT",
-    scalaVersion := "2.12.4"
+    scalaVersion := "2.12.6"
   )
 }
