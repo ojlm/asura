9a6b1550d4fa2156c64e0a9d839159a97eaa09f5
zhengshaodong <ngxcorpio@gmail.com>
2018-06-20 10:54:38 +0800

    fix common validator

--- asura-core/src/main/scala/asura/core/cs/CommonValidator.scala
+++ asura-core/src/main/scala/asura/core/cs/CommonValidator.scala
@@ -8,7 +8,7 @@ object CommonValidator {
     if (StringUtils.isEmpty(id)) {
       false
     } else {
-      id.forall(c => Character.isLetterOrDigit(c) || c.equals('_') || c.equals('-') || c.equals('.'))
+      id.forall(c => (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c.equals('_') || c.equals('-') || c.equals('.'))
     }
   }
 }
