825681c0e2f48ebddfb58494af5e9691a96d5470
zhengshaodong <ngxcorpio@gmail.com>
2018-08-04 01:46:14 +0800

    add body parser and result generator

--- app/asura/app/api/BaseApi.scala
+++ app/asura/app/api/BaseApi.scala
@@ -1,8 +1,25 @@
 package asura.app.api
 
-import play.api.mvc.InjectedController
+import akka.util.ByteString
+import asura.common.model.ApiRes
+import asura.common.util.JsonUtils
+import play.api.http.{ContentTypes, HttpEntity}
+import play.api.mvc.{InjectedController, Request, ResponseHeader, Result}
 
 
 trait BaseApi extends InjectedController {
+
+  implicit class JsonToClass(req: Request[String]) {
+    def bodyAs[T <: AnyRef](c: Class[T]): T = JsonUtils.parse[T](req.body, c)
+  }
+
+  object OkApiRes {
+    def apply(apiRes: ApiRes): Result = {
+      Result(
+        header = ResponseHeader(200),
+        HttpEntity.Strict(ByteString(JsonUtils.stringify(apiRes)), Some(ContentTypes.JSON))
+      )
+    }
+  }
 
 }
