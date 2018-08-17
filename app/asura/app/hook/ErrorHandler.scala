be1c92ba513c5e55bca9baedbdefdd95dec2dd1b
zhengshaodong <ngxcorpio@gmail.com>
2018-08-17 18:06:02 +0800

    i18n support

--- app/asura/app/hook/ErrorHandler.scala
+++ app/asura/app/hook/ErrorHandler.scala
@@ -1,17 +1,19 @@
 package asura.app.hook
 
 import asura.app.api.BaseApi.OkApiRes
-import asura.common.model.ApiResError
+import asura.common.model.{ApiCode, ApiRes, ApiResError}
 import asura.common.util.{LogUtils, StringUtils}
-import javax.inject.Singleton
+import asura.core.ErrorMessages.ErrorMessageException
+import javax.inject.{Inject, Singleton}
 import org.slf4j.LoggerFactory
 import play.api.http.HttpErrorHandler
+import play.api.i18n.{Langs, MessagesApi}
 import play.api.mvc.{RequestHeader, Result}
 
 import scala.concurrent.Future
 
 @Singleton
-class ErrorHandler extends HttpErrorHandler {
+class ErrorHandler @Inject()(messagesApi: MessagesApi, langs: Langs) extends HttpErrorHandler {
 
   lazy val logger = LoggerFactory.getLogger(classOf[ErrorHandler])
 
@@ -22,6 +24,18 @@ class ErrorHandler extends HttpErrorHandler {
   override def onServerError(request: RequestHeader, exception: Throwable): Future[Result] = {
     val logStack = LogUtils.stackTraceToString(exception)
     logger.warn(logStack)
-    Future.successful(OkApiRes(ApiResError(exception.getMessage)))
+    exception match {
+      case errMsgException: ErrorMessageException =>
+        val requestLocal = request.headers.get("Local")
+        implicit val lang = if (requestLocal.nonEmpty) {
+          langs.availables.find(_.code == requestLocal.get).getOrElse(langs.availables.head)
+        } else {
+          langs.availables.head
+        }
+        val errMsg = messagesApi(errMsgException.error.name, errMsgException.error.errMsg)
+        Future.successful(OkApiRes(ApiRes(code = ApiCode.ERROR, msg = errMsg, data = logStack)))
+      case _ =>
+        Future.successful(OkApiRes(ApiRes(code = ApiCode.ERROR, msg = exception.getMessage, data = logStack)))
+    }
   }
 }
