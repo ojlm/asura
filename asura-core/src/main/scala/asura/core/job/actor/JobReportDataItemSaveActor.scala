8ef9563d51d68ca5fa05e313e53f79f0bce40228
zhengshaodong <ngxcorpio@gmail.com>
2018-09-26 17:37:36 +0800

    actor for asynchronously saving detail data of job report

---
+++ asura-core/src/main/scala/asura/core/job/actor/JobReportDataItemSaveActor.scala
@@ -0,0 +1,54 @@
+package asura.core.job.actor
+
+import akka.actor.Props
+import asura.common.actor.BaseActor
+import asura.core.es.model.JobReportDataItem
+import asura.core.es.service.JobReportDataService
+import asura.core.job.actor.JobReportDataItemSaveActor.{Flush, SaveReportDataItemMessage}
+
+import scala.collection.mutable.ArrayBuffer
+import scala.concurrent.duration._
+
+class JobReportDataItemSaveActor(dayIndexSuffix: String) extends BaseActor {
+
+  val messages = ArrayBuffer[SaveReportDataItemMessage]()
+
+  override def receive: Receive = {
+    case m: SaveReportDataItemMessage =>
+      messages += m
+      if (messages.length >= 10) {
+        insert()
+      }
+      context.system.scheduler.scheduleOnce(1 seconds) {
+        self ! Flush
+      }(context.system.dispatcher)
+    case Flush =>
+      insert()
+  }
+
+  override def preStart(): Unit = {
+  }
+
+  override def postStop(): Unit = {
+    insert()
+    log.debug(s"${self.path} is stopped")
+  }
+
+  private def insert(): Unit = {
+    if (messages.length > 0) {
+      log.info(s"${messages.length} items is saving...")
+      JobReportDataService.index(messages, dayIndexSuffix)
+      messages.clear()
+    }
+  }
+}
+
+object JobReportDataItemSaveActor {
+
+  def props(dayIndexSuffix: String) = Props(new JobReportDataItemSaveActor(dayIndexSuffix))
+
+  case class Flush()
+
+  case class SaveReportDataItemMessage(id: String, dataItem: JobReportDataItem)
+
+}
