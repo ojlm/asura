a0e8b7675f74591a400e96fa04c46e83701dc866
zhengshaodong <ngxcorpio@gmail.com>
2018-09-27 07:57:36 +0800

    retrieve specified fields in source

--- asura-core/src/main/scala/asura/core/cs/model/QueryJobReport.scala
+++ asura-core/src/main/scala/asura/core/cs/model/QueryJobReport.scala
@@ -3,6 +3,8 @@ package asura.core.cs.model
 case class QueryJobReport(
                            scheduler: String,
                            group: String,
+                           project: String,
                            classAlias: String,
+                           text: String,
                            `type`: String,
                          ) extends QueryPage
