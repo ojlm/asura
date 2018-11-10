6dd411ab9229214a67a3ae7a7b5d3b5e6093987a
zhengshaodong <ngxcorpio@gmail.com>
2018-11-10 17:34:22 +0800

    update build script

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-docker build --file ./Dockerfile -t indigo-api ../
-docker tag indigo-api asurapro/indigo-api
+docker build --file ./Dockerfile -t asurapro/indigo-api ../
 docker push asurapro/indigo-api
