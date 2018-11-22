304a8a3f3075a2f776ad9f6cbf9de59aff375dc0
zhengshaodong <ngxcorpio@gmail.com>
2018-11-22 20:16:14 +0800

    add activity aggregation service

---
+++ asura-app/app/asura/app/api/ActivityApi.scala
@@ -0,0 +1,30 @@
+package asura.app.api
+
+import akka.actor.ActorSystem
+import asura.app.api.BaseApi.OkApiRes
+import asura.common.model.ApiRes
+import asura.core.cs.model.AggsQuery
+import asura.core.es.model.Activity
+import asura.core.es.service._
+import javax.inject.{Inject, Singleton}
+import org.pac4j.play.scala.SecurityComponents
+
+import scala.concurrent.ExecutionContext
+
+@Singleton
+class ActivityApi @Inject()(implicit system: ActorSystem,
+                            val exec: ExecutionContext,
+                            val controllerComponents: SecurityComponents
+                           ) extends BaseApi {
+
+  def trend(groups: Boolean = true) = Action(parse.byteString).async { implicit req =>
+    val aggs = req.bodyAs(classOf[AggsQuery])
+    aggs.types = Seq(Activity.TYPE_TEST_CASE)
+    val res = for {
+      trends <- ActivityService.trend(aggs)
+    } yield trends
+    res.map(trends => {
+      OkApiRes(ApiRes(data = Map("trends" -> trends)))
+    })
+  }
+}
