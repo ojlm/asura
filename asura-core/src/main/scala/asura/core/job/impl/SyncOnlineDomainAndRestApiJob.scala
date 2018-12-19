a8d8b57a237f39bfda91de01045479f1f6e9dc87
zhengshaodong <ngxcorpio@gmail.com>
2018-12-19 11:23:42 +0800

    Log debug information

--- asura-core/src/main/scala/asura/core/job/impl/SyncOnlineDomainAndRestApiJob.scala
+++ asura-core/src/main/scala/asura/core/job/impl/SyncOnlineDomainAndRestApiJob.scala
@@ -3,6 +3,7 @@ package asura.core.job.impl
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
+import asura.common.util.LogUtils
 import asura.core.CoreConfig
 import asura.core.es.model.RestApiOnlineLog.GroupProject
 import asura.core.es.model._
@@ -22,67 +23,73 @@ class SyncOnlineDomainAndRestApiJob extends Job {
   val logger = Logger(classOf[SyncOnlineDomainAndRestApiJob])
 
   override def execute(context: JobExecutionContext): Unit = {
-    val detail = context.getJobDetail
-    val dayCount = detail.getJobDataMap.getInt(KEY_DAY)
-    val domainCount = detail.getJobDataMap.getInt(KEY_DOMAIN_COUNT)
-    val jobApiCount = detail.getJobDataMap.getInt(KEY_API_COUNT)
-    if (domainCount > 0) {
-      val projectCoverageLogs = ArrayBuffer[ProjectApiCoverage]()
-      val yesterday = LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern(CoreConfig.onlineLogDatePattern))
-      import asura.common.util.FutureUtils.RichFuture
-      val domainLogs = DomainOnlineLogService.getOnlineDomain(domainCount, yesterday).await
-      domainLogs.foreach(domainCountLog => {
-        // get all apis of each domain
-        val apiLogs = OnlineRequestLogService.getOnlineApi(domainCountLog.name, domainCountLog.count, jobApiCount).await
-        if (apiLogs.nonEmpty) {
-          // get all projects of each domain
-          val projects = ProjectService.getProjectsByDomain(domainCountLog.name).await
-          if (projects.nonEmpty) {
-            // key: {method}{urlPath}
-            val apiMap = mutable.HashMap[String, RestApiOnlineLog]()
-            val apisShouldQuery = ArrayBuffer[Query]()
-            apiLogs.foreach(apiLog => {
-              // every log should have a copy of (group, project, covered)
-              apiLog.belongs = projects.map(p => GroupProject(p.group, p.id))
-              apiMap += (s"${apiLog.method}${apiLog.urlPath}" -> apiLog)
-              apisShouldQuery += boolQuery().must(
-                termQuery(FieldKeys.FIELD_OBJECT_REQUEST_METHOD, apiLog.method),
-                termQuery(FieldKeys.FIELD_OBJECT_REQUEST_URLPATH, apiLog.urlPath)
-              )
-            })
-            val domainApiSet = mutable.HashMap[String, Long]()
-            projects.foreach(project => {
-              // get all apis of each project
-              val projectApiSet = CaseService.getApiSet(project, apisShouldQuery, apiMap.size).await
-              projectApiSet.foreach(projectApiItem => {
-                domainApiSet += projectApiItem
-                apiMap.get(projectApiItem._1).foreach(apiLog => {
-                  apiLog.belongs
-                    .filter(belong => belong.group == project.group && belong.project == project.id)
-                    .foreach(belong => {
-                      belong.covered = true
-                      belong.count = projectApiItem._2
-                    })
-                })
+    try {
+      val detail = context.getJobDetail
+      val dayCount = detail.getJobDataMap.getInt(KEY_DAY)
+      val domainCount = detail.getJobDataMap.getInt(KEY_DOMAIN_COUNT)
+      val jobApiCount = detail.getJobDataMap.getInt(KEY_API_COUNT)
+      if (domainCount > 0) {
+        val projectCoverageLogs = ArrayBuffer[ProjectApiCoverage]()
+        val yesterday = LocalDate.now().minusDays(1).format(DateTimeFormatter.ofPattern(CoreConfig.onlineLogDatePattern))
+        import asura.common.util.FutureUtils.RichFuture
+        val domainLogs = DomainOnlineLogService.getOnlineDomain(domainCount, yesterday).await
+        logger.debug(s"online domain count: ${domainLogs.size}")
+        domainLogs.foreach(domainCountLog => {
+          // get all apis of each domain
+          val apiLogs = OnlineRequestLogService.getOnlineApi(domainCountLog.name, domainCountLog.count, jobApiCount).await
+          logger.debug(s"online api count of ${domainCountLog.name}: ${apiLogs.size}")
+          if (apiLogs.nonEmpty) {
+            // get all projects of each domain
+            val projects = ProjectService.getProjectsByDomain(domainCountLog.name).await
+            if (projects.nonEmpty) {
+              // key: {method}{urlPath}
+              val apiMap = mutable.HashMap[String, RestApiOnlineLog]()
+              val apisShouldQuery = ArrayBuffer[Query]()
+              apiLogs.foreach(apiLog => {
+                // every log should have a copy of (group, project, covered)
+                apiLog.belongs = projects.map(p => GroupProject(p.group, p.id))
+                apiMap += (s"${apiLog.method}${apiLog.urlPath}" -> apiLog)
+                apisShouldQuery += boolQuery().must(
+                  termQuery(FieldKeys.FIELD_OBJECT_REQUEST_METHOD, apiLog.method),
+                  termQuery(FieldKeys.FIELD_OBJECT_REQUEST_URLPATH, apiLog.urlPath)
+                )
               })
-              projectCoverageLogs += ProjectApiCoverage(
-                group = project.group,
-                project = project.id,
-                domain = domainCountLog.name,
-                date = yesterday,
-                coverage = Math.round((projectApiSet.size * 10000L).toDouble / apiMap.size.toDouble).toInt
-              )
-            })
-            domainCountLog.coverage = Math.round((domainApiSet.size * 10000L).toDouble / apiMap.size.toDouble).toInt
+              val domainApiSet = mutable.HashMap[String, Long]()
+              projects.foreach(project => {
+                // get all apis of each project
+                val projectApiSet = CaseService.getApiSet(project, apisShouldQuery, apiMap.size).await
+                projectApiSet.foreach(projectApiItem => {
+                  domainApiSet += projectApiItem
+                  apiMap.get(projectApiItem._1).foreach(apiLog => {
+                    apiLog.belongs
+                      .filter(belong => belong.group == project.group && belong.project == project.id)
+                      .foreach(belong => {
+                        belong.covered = true
+                        belong.count = projectApiItem._2
+                      })
+                  })
+                })
+                projectCoverageLogs += ProjectApiCoverage(
+                  group = project.group,
+                  project = project.id,
+                  domain = domainCountLog.name,
+                  date = yesterday,
+                  coverage = Math.round((projectApiSet.size * 10000L).toDouble / apiMap.size.toDouble).toInt
+                )
+              })
+              domainCountLog.coverage = Math.round((domainApiSet.size * 10000L).toDouble / apiMap.size.toDouble).toInt
+            }
+            RestApiOnlineLogService.index(apiLogs, domainCountLog.date).await
           }
-          RestApiOnlineLogService.index(apiLogs, domainCountLog.date).await
-        }
-      })
-      if (domainLogs.nonEmpty) DomainOnlineLogService.index(domainLogs)
-      if (projectCoverageLogs.nonEmpty) ProjectApiCoverageService.index(projectCoverageLogs)
-    }
-    if (dayCount > 0) {
-      deleteOutdatedIndices(dayCount)
+        })
+        if (domainLogs.nonEmpty) DomainOnlineLogService.index(domainLogs).await
+        if (projectCoverageLogs.nonEmpty) ProjectApiCoverageService.index(projectCoverageLogs).await
+      }
+      if (dayCount > 0) {
+        deleteOutdatedIndices(dayCount)
+      }
+    } catch {
+      case t: Throwable => logger.error(LogUtils.stackTraceToString(t))
     }
   }
 
