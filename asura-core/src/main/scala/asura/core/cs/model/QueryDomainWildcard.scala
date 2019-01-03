a388c4bb8e086619bbe8f4ea5d0793ce51eaea0b
zhengshaodong <ngxcorpio@gmail.com>
2019-01-03 15:21:37 +0800

    Add route for searching domain by wildcard

---
+++ asura-core/src/main/scala/asura/core/cs/model/QueryDomainWildcard.scala
@@ -0,0 +1,6 @@
+package asura.core.cs.model
+
+case class QueryDomainWildcard(
+                                var date: String,
+                                val domain: String,
+                              ) extends QueryPage
