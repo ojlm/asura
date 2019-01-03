a04137fc704cfad26d849a8ec2c4400c0f9612ce
zhengshaodong <ngxcorpio@gmail.com>
2019-01-03 20:40:41 +0800

    Add tag to query model

--- asura-core/src/main/scala/asura/core/cs/model/QueryDomainWildcard.scala
+++ asura-core/src/main/scala/asura/core/cs/model/QueryDomainWildcard.scala
@@ -3,4 +3,5 @@ package asura.core.cs.model
 case class QueryDomainWildcard(
                                 var date: String,
                                 val domain: String,
+                                val tag: String,
                               ) extends QueryPage
