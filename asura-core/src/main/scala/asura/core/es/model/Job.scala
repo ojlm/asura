7456309bf68949ec305ad83e484584417a2bef70
zhengshaodong <ngxcorpio@gmail.com>
2019-01-18 15:09:52 +0800

    Fix job model update

--- asura-core/src/main/scala/asura/core/es/model/Job.scala
+++ asura-core/src/main/scala/asura/core/es/model/Job.scala
@@ -2,6 +2,7 @@ package asura.core.es.model
 
 import asura.common.util.StringUtils
 import asura.core.es.EsConfig
+import asura.core.util.JacksonSupport
 import com.sksamuel.elastic4s.mappings._
 
 import scala.collection.mutable
@@ -21,19 +22,46 @@ case class Job(
                 var updatedAt: String = null,
               ) extends BaseIndex {
 
-  override def toUpdateMap: Map[String, Any] = {
+  override def toUpdateScriptParams: (String, Map[String, Any]) = {
+    val sb = StringBuilder.newBuilder
     val m = mutable.Map[String, Any]()
-    checkCommFieldsToUpdate(m)
-    if (null != trigger) {
-      m += (FieldKeys.FIELD_TRIGGER -> trigger)
-    }
-    if (null != jobData) {
-      m += (FieldKeys.FIELD_JOB_DATA -> jobData)
-    }
+    checkCommFieldsToUpdate(m, sb)
     if (null != env) {
       m += (FieldKeys.FIELD_ENV -> env)
+      addScriptUpdateItem(sb, FieldKeys.FIELD_ENV)
     }
-    m.toMap
+    addScriptUpdateItem(sb, FieldKeys.FIELD_TRIGGER)
+    if (null != trigger) {
+      m += (FieldKeys.FIELD_TRIGGER -> JacksonSupport.mapper.convertValue(trigger, classOf[java.util.List[Map[String, Any]]]))
+    } else {
+      m += (FieldKeys.FIELD_TRIGGER -> Nil)
+    }
+    addScriptUpdateItem(sb, FieldKeys.FIELD_JOB_DATA)
+    if (null != jobData) {
+      m += (FieldKeys.FIELD_JOB_DATA -> jobDataToMap())
+    } else {
+      m += (FieldKeys.FIELD_JOB_DATA -> Map.empty)
+    }
+    (sb.toString, m.toMap)
+  }
+
+  private def jobDataToMap(): Map[String, Object] = {
+    val cs = if (null != jobData.cs) {
+      JacksonSupport.mapper.convertValue(jobData.cs, classOf[java.util.List[Map[String, Any]]])
+    } else {
+      Nil
+    }
+    val scenario = if (null != jobData.scenario) {
+      JacksonSupport.mapper.convertValue(jobData.scenario, classOf[java.util.List[Map[String, Any]]])
+    } else {
+      Nil
+    }
+    val ext = if (null != jobData.ext) {
+      JacksonSupport.mapper.convertValue(jobData.ext, classOf[Map[String, Any]])
+    } else {
+      null
+    }
+    Map(FieldKeys.FIELD_CS -> cs, FieldKeys.FIELD_SCENARIO -> scenario, FieldKeys.FIELD_EXT -> ext)
   }
 }
 
