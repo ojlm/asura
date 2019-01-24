d9073770a792a4ba5e0ec962eb85be048481b280
zhengshaodong <ngxcorpio@gmail.com>
2019-01-24 20:14:55 +0800

    Actor for cache curator client and dubbo reference config

--- asura-dubbo/src/main/scala/asura/dubbo/DubboConfig.scala
+++ asura-dubbo/src/main/scala/asura/dubbo/DubboConfig.scala
@@ -1,6 +1,10 @@
 package asura.dubbo
 
+import java.util.concurrent.Executors
+
 import com.alibaba.dubbo.config.ApplicationConfig
+
+import scala.concurrent.ExecutionContext
 
 case class DubboConfig(
                         appName: String = "asura-dubbo"
@@ -14,6 +18,10 @@ object DubboConfig {
   val DEFAULT_PORT = 20880
   val DEFAULT_ROOT_DUBBO_PATH = "/dubbo"
   val DEFAULT_PROMPT = "dubbo>"
+  val DEFAULT_ZK_CLIENT_CACHE_SIZE = 10
+  val DEFAULT_DUBBO_REF_CACHE_SIZE = 20
   var appName = "asura-dubbo"
   var appConfig = new ApplicationConfig(appName)
+
+  val DUBBO_EC = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(4))
 }
--- asura-dubbo/src/main/scala/asura/dubbo/actor/GenericServiceInvokerActor.scala
+++ asura-dubbo/src/main/scala/asura/dubbo/actor/GenericServiceInvokerActor.scala
@@ -1,13 +1,11 @@
 package asura.dubbo.actor
 
 import akka.actor.{Props, Status}
-import akka.pattern.pipe
+import akka.pattern.{ask, pipe}
 import akka.util.Timeout
 import asura.common.actor.BaseActor
-import asura.common.util.{LogUtils, StringUtils}
+import asura.common.util.LogUtils
 import asura.dubbo.actor.GenericServiceInvokerActor.{GetInterfaceMethodParams, GetInterfacesMessage, GetProvidersMessage}
-import asura.dubbo.cache.{CuratorClientCache, ReferenceCache}
-import asura.dubbo.model.{DubboInterface, DubboProvider}
 import asura.dubbo.{DubboConfig, GenericRequest}
 
 import scala.concurrent.duration._
@@ -18,13 +16,16 @@ class GenericServiceInvokerActor extends BaseActor {
   implicit val ec: ExecutionContext = context.dispatcher
   implicit val timeout: Timeout = 30.seconds
 
+  val curatorClientCacheActor = context.actorOf(CuratorClientCacheActor.props())
+  val referenceActor = context.actorOf(DubboReferenceCacheActor.props())
+
   override def receive: Receive = {
-    case GetInterfacesMessage(zkAddr, path) =>
-      getInterfaces(zkAddr, path) pipeTo sender()
-    case GetProvidersMessage(zkAddr, path, ref) =>
-      getProviders(zkAddr, path, ref) pipeTo sender()
-    case request: GenericRequest =>
-      test(request) pipeTo sender()
+    case msg: GetInterfacesMessage =>
+      curatorClientCacheActor ? msg pipeTo sender()
+    case msg: GetProvidersMessage =>
+      curatorClientCacheActor ? msg pipeTo sender()
+    case msg: GenericRequest =>
+      referenceActor ? msg pipeTo sender()
     case msg: GetInterfaceMethodParams =>
       context.actorOf(InterfaceMethodParamsActor.props(sender(), msg))
     case Status.Failure(t) =>
@@ -34,32 +35,8 @@ class GenericServiceInvokerActor extends BaseActor {
       Future.failed(new RuntimeException("Unknown message type")) pipeTo sender()
   }
 
-  def getInterfaces(zkAddr: String, path: String): Future[Seq[DubboInterface]] = {
-    val dubboPath = if (StringUtils.isEmpty(path)) {
-      DubboConfig.DEFAULT_ROOT_DUBBO_PATH
-    } else {
-      path
-    }
-    CuratorClientCache.getChildren(zkAddr, dubboPath).map(items => {
-      items.map(DubboInterface(zkAddr, dubboPath, _))
-    })
-  }
-
-  def getProviders(zkAddr: String, path: String, ref: String): Future[Seq[DubboProvider]] = {
-    CuratorClientCache.getInterfaceProviders(zkAddr, ref)
-  }
-
-  def test(request: GenericRequest): Future[Any] = {
-    // TODO: clear cache when timeout
-    val (service, refConfig) = ReferenceCache.getServiceAndConfig(request)
-    Future {
-      // https://github.com/apache/incubator-dubbo/issues/3163
-      service.$invoke(request.method, request.getParameterTypes(), request.getArgs())
-    }(ExecutionContext.global)
-  }
-
   override def postStop(): Unit = {
-    CuratorClientCache.destroy()
+    DubboConfig.DUBBO_EC.shutdown()
   }
 }
 
