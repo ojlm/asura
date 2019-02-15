db0469b43c2cfec9f5c5a017dcb60614ac23c53a
zhengshaodong <ngxcorpio@gmail.com>
2019-02-15 18:00:40 +0800

    Add ClusterManagerActor for member monitor

---
+++ asura-cluster/src/main/scala/asura/cluster/model/MemberInfo.scala
@@ -0,0 +1,25 @@
+package asura.cluster.model
+
+import akka.cluster.Member
+import asura.common.util.StringUtils
+
+case class MemberInfo(
+                       roles: Seq[String],
+                       address: String,
+                       protocol: String,
+                       port: Int
+                     ) {
+
+}
+
+object MemberInfo {
+
+  def fromMember(m: Member): MemberInfo = {
+    MemberInfo(
+      roles = m.roles.toSeq,
+      protocol = m.address.protocol,
+      address = m.address.host.getOrElse(StringUtils.EMPTY),
+      port = m.address.port.getOrElse(0)
+    )
+  }
+}
