9f686b71623eb231d56d952e6e824ace90de72bd
zhengshaodong <ngxcorpio@gmail.com>
2019-04-23 23:59:45 +0800

    Add actor for caching mysql connections

--- asura-core/src/main/scala/asura/core/sql/SqlConfig.scala
+++ asura-core/src/main/scala/asura/core/sql/SqlConfig.scala
@@ -1,6 +1,13 @@
 package asura.core.sql
 
+import java.util.concurrent.Executors
+
+import scala.concurrent.ExecutionContext
+
 object SqlConfig {
 
   val MAX_ROWS_SIZE = 200
+  val DEFAULT_MYSQL_CONNECTOR_CACHE_SIZE = 10
+
+  val SQL_EC = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(4))
 }
