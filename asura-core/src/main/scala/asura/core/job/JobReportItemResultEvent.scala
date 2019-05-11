031dc14aa1cb8aed85dd6da8f33ffa068e08f6ca
zhengshaodong <ngxcorpio@gmail.com>
2019-05-11 23:56:19 +0800

    Send event of step result to front ws actor

--- asura-core/src/main/scala/asura/core/job/JobReportItemResultEvent.scala
+++ asura-core/src/main/scala/asura/core/job/JobReportItemResultEvent.scala
@@ -1,9 +1,11 @@
 package asura.core.job
 
+import asura.core.runtime.AbstractResult
+
 /**
   * @param result :
   *               [[asura.core.http.HttpResult]],
   *               [[asura.core.sql.SqlResult]],
   *               [[asura.core.dubbo.DubboResult]]
   */
-case class JobReportItemResultEvent(index: Int, status: String, errMsg: String = null, result: Any = null)
+case class JobReportItemResultEvent(index: Int, status: String, errMsg: String = null, result: AbstractResult = null)
