bb13a4719fd8f2256b2a0012e43ae2b2e1c8d42d
zhengshaodong <ngxcorpio@gmail.com>
2019-05-11 17:28:50 +0800

    Remove useless parameter

--- asura-core/src/main/scala/asura/core/assertion/engine/HttpResponseAssert.scala
+++ asura-core/src/main/scala/asura/core/assertion/engine/HttpResponseAssert.scala
@@ -54,6 +54,6 @@ object HttpResponseAssert {
       },
       entity
     )
-    HttpResult.eval(docId, response, assert, caseContext, caseRequest, caseResponse)
+    HttpResult.eval(docId, assert, caseContext, caseRequest, caseResponse)
   }
 }
