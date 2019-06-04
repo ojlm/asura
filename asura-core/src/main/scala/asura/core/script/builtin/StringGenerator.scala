c51d0e887189518525347dc835cf4d0664bbbd16
zhengshaodong <ngxcorpio@gmail.com>
2019-06-04 18:56:32 +0800

    Add randomAlphabetic and randomNumeric function

--- asura-core/src/main/scala/asura/core/script/builtin/StringGenerator.scala
+++ asura-core/src/main/scala/asura/core/script/builtin/StringGenerator.scala
@@ -8,11 +8,23 @@ object StringGenerator {
       |function random(count) { return RandomStringUtils.randomAlphanumeric(count);}
     """.stripMargin
 
+  val randomAlphabetic =
+    """
+      |var RandomStringUtils = Java.type('org.apache.commons.lang3.RandomStringUtils');
+      |function randomAlphabetic(count) { return RandomStringUtils.randomAlphabetic(count);}
+    """.stripMargin
+
+  val randomNumeric =
+    """
+      |var RandomStringUtils = Java.type('org.apache.commons.lang3.RandomStringUtils');
+      |function randomNumeric(count) { return RandomStringUtils.randomNumeric(count);}
+    """.stripMargin
+
   val UUID =
     """
       |var UUID = Java.type('java.util.UUID');
       |function uuid() { return UUID.randomUUID().toString();}
     """.stripMargin
 
-  def exports = Seq(random, UUID).mkString
+  def exports = Seq(random, UUID, randomAlphabetic, randomNumeric).mkString
 }
