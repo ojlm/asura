9dfc401c837064cc474b754eafc492a74bc51a9d
zhengshaodong <ngxcorpio@gmail.com>
2019-07-01 13:19:02 +0800

    Fix test code

--- asura-pea/test/asura/pea/gatling/RunnerSpec.scala
+++ asura-pea/test/asura/pea/gatling/RunnerSpec.scala
@@ -4,13 +4,17 @@ import asura.pea.actor.GatlingRunnerActor
 import asura.pea.actor.GatlingRunnerActor.StartMessage
 import asura.pea.{IDEPathHelper, PeaAppSpec}
 
+import scala.concurrent.ExecutionContext.global
+
 object RunnerSpec extends PeaAppSpec {
+
+  implicit val ec = global
 
   def main(args: Array[String]): Unit = {
     val message = StartMessage(
       IDEPathHelper.binariesFolder.toAbsolutePath.toString,
       IDEPathHelper.resultsFolder.toAbsolutePath.toString,
-      "simulations.BasicSimulation"
+      "simulations.BasicSimulation",
     )
     val code = GatlingRunnerActor.start(message)
     logger.info(s"Exit: ${code}")
