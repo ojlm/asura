3e9e25733a340bfdb487b5e87afd8413c06811ca
zhengshaodong <ngxcorpio@gmail.com>
2019-07-09 17:44:31 +0800

    Refactor actor for job ci

--- asura-app/app/asura/app/api/ci/CiApi.scala
+++ asura-app/app/asura/app/api/ci/CiApi.scala
@@ -1,4 +1,4 @@
-package asura.app.api
+package asura.app.api.ci
 
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.Source
@@ -11,7 +11,7 @@ import org.pac4j.play.scala.SecurityComponents
 import play.api.http.ContentTypes
 import play.api.libs.EventSource
 import play.api.libs.streams.ActorFlow
-import play.api.mvc.WebSocket
+import play.api.mvc.{Codec, WebSocket}
 
 import scala.concurrent.ExecutionContext
 
@@ -23,11 +23,17 @@ class CiApi @Inject()(
                        val controllerComponents: SecurityComponents
                      ) extends BaseApi {
 
-  def v1(id: String) = WebSocket.accept[String, String] { implicit req =>
+  implicit val codec = Codec.utf_8
+
+  def home() = Action {
+    Ok("ci")
+  }
+
+  def jobWS(id: String) = WebSocket.accept[String, String] { implicit req =>
     ActorFlow.actorRef(out => JobCiActor.props(id, out))
   }
 
-  def v2(id: String) = Action {
+  def jobSSE(id: String) = Action {
     val ciActor = system.actorOf(JobCiActor.props(id))
     val source = Source.actorRef[String](BaseApi.DEFAULT_SOURCE_BUFFER_SIZE, OverflowStrategy.dropHead)
       .mapMaterializedValue(ref => ciActor ! SenderMessage(ref))
