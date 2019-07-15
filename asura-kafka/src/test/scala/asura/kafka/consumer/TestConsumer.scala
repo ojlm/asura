b19a4dd9fa52f2076d7c6a2971fd1b73a4221579
zhengshaodong <ngxcorpio@gmail.com>
2019-07-15 11:45:42 +0800

    Update configuration

--- asura-kafka/src/test/scala/asura/kafka/consumer/TestConsumer.scala
+++ asura-kafka/src/test/scala/asura/kafka/consumer/TestConsumer.scala
@@ -19,11 +19,13 @@ object TestConsumer extends StrictLogging {
     implicit val ec = system.dispatcher
 
     val consumerSettings = ConsumerSettings(system, new StringDeserializer, new StringDeserializer)
+      .withGroupId("test-group1")
 
     val done = Consumer
       .plainSource(consumerSettings, Subscriptions.topics("test-topic"))
-      .runWith(Sink.foreach(record => logger.info(record.toString)))
-
+      .runWith(Sink.foreach(record =>
+        logger.info(s"topic:${record.topic()}, partition:${record.partition()}, offset:${record.offset()}, key:${record.key()}, value: ${record.value()}"))
+      )
     done onComplete {
       case scala.util.Success(_) => logger.info("Done"); system.terminate()
       case scala.util.Failure(err) => logger.error(err.toString); system.terminate()
