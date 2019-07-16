9ea347f8f23fe09e617406e7b6dac5785c2ea23e
zhengshaodong <ngxcorpio@gmail.com>
2019-07-16 09:59:21 +0800

    Fix missing head zero

--- asura-common/src/main/scala/asura/common/util/MD5Utils.scala
+++ asura-common/src/main/scala/asura/common/util/MD5Utils.scala
@@ -4,9 +4,20 @@ import java.security.MessageDigest
 
 object MD5Utils {
 
-  def encode(plainText: String): String = {
+  val HEX_ARRAY = "0123456789abcdef".toCharArray()
+
+  def encodeToHex(plainText: String): String = {
     val md5Bytes = MessageDigest.getInstance("MD5").digest(plainText.getBytes)
-    val sb = new StringBuilder(BigInt(1, md5Bytes).toString(16))
-    sb.toString
+    bytesToHex(md5Bytes)
+  }
+
+  def bytesToHex(bytes: Array[Byte]): String = {
+    val hexChars = new Array[Char](bytes.length * 2)
+    for (i <- 0 until bytes.length) {
+      val v = bytes(i) & 0xFF
+      hexChars(i * 2) = HEX_ARRAY(v >>> 4)
+      hexChars(i * 2 + 1) = HEX_ARRAY(v & 0x0F)
+    }
+    new String(hexChars)
   }
 }
