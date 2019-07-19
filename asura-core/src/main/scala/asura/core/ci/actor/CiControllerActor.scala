78e9b3e1118a8d42c7f2603665ba160304d71004
zhengshaodong <ngxcorpio@gmail.com>
2019-07-19 19:16:05 +0800

    Actor that handles ci events

--- asura-core/src/main/scala/asura/core/ci/actor/CiControllerActor.scala
+++ asura-core/src/main/scala/asura/core/ci/actor/CiControllerActor.scala
@@ -2,12 +2,17 @@ package asura.core.ci.actor
 
 import akka.actor.Props
 import asura.common.actor.BaseActor
+import asura.common.util.StringUtils
 import asura.core.ci.CiTriggerEventMessage
 
 class CiControllerActor extends BaseActor {
 
   override def receive: Receive = {
-    case CiTriggerEventMessage =>
+    case msg: CiTriggerEventMessage =>
+      log.debug(msg.toString)
+      if (StringUtils.isNotEmpty(msg.group) && StringUtils.isNotEmpty(msg.project)) {
+
+      }
   }
 }
 
