37b809670612109c1c1f5452005aa7d96bac985c
zhengshaodong <ngxcorpio@gmail.com>
2019-09-24 20:25:50 +0800

    Support indigo frontend resources

--- asura-app/app/asura/app/api/HomeApi.scala
+++ asura-app/app/asura/app/api/HomeApi.scala
@@ -5,14 +5,32 @@ import asura.core.es.model._
 import asura.core.es.service.HomeService
 import asura.core.model.QueryHome
 import asura.play.api.BaseApi.OkApiRes
+import controllers.Assets
 import javax.inject.{Inject, Singleton}
 import org.pac4j.play.scala.SecurityComponents
+import play.api.http.HttpErrorHandler
+import play.api.mvc.{Action, AnyContent}
 
 import scala.concurrent.ExecutionContext
 
 @Singleton
-class HomeApi @Inject()(implicit exec: ExecutionContext, val controllerComponents: SecurityComponents)
+class HomeApi @Inject()(
+                         implicit exec: ExecutionContext,
+                         val controllerComponents: SecurityComponents,
+                         val assets: Assets,
+                         val errorHandler: HttpErrorHandler,
+                       )
   extends BaseApi {
+
+  def index() = assets.at("index.html")
+
+  def asset(resource: String): Action[AnyContent] = {
+    if (resource.startsWith("api") || resource.startsWith("openapi")) {
+      Action.async(r => errorHandler.onClientError(r, NOT_FOUND, "Not found"))
+    } else {
+      if (resource.contains(".")) assets.at(resource) else index
+    }
+  }
 
   def query() = Action(parse.byteString).async { implicit req =>
     val queryHome = req.bodyAs(classOf[QueryHome])
