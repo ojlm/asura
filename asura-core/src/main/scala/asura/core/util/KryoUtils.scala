d9b2e3992d9f5dcafcb6168c016664bbd3b0705a
xcorpio <ngxcorpio@gmail.com>
2021-01-29 10:37:14 +0800

    Add kryo

---
+++ asura-core/src/main/scala/asura/core/util/KryoUtils.scala
@@ -0,0 +1,27 @@
+package asura.core.util
+
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.io.{Input, Output}
+
+object KryoUtils {
+
+  private val localInstance: ThreadLocal[Kryo] = ThreadLocal.withInitial(() => {
+    val kryo: Kryo = new Kryo()
+    kryo.setRegistrationRequired(false)
+    kryo
+  })
+
+  def toBytes(obj: Object): Array[Byte] = {
+    val output = new Output(1024, -1)
+    localInstance.get.writeObject(output, obj)
+    output.toBytes
+  }
+
+  def fromBytes[T](bytes: Array[Byte], clazz: Class[T]): T = {
+    val input = new Input(bytes, 0, bytes.length)
+    localInstance.get.readObject(input, clazz)
+  }
+
+  def get: Kryo = localInstance.get
+
+}
